c5eb43d40632a134e1976a3ef028c82192a02c36
Alyson Burton <funkyfrog3000@Alyson-Burtons-MacBook-Pro.local>
2016-08-24 17:06:03 -0700

    -

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var words = [
   i = 0;
 
 setInterval(function() {         // \/ \/ callback function
-  $('#wordChanger').fadeOut(400, function() {
+  $('#wordChanger').fadeOut(800, function() {
                       // if i = last index ? i = 0 else i++
     $(this).text(words[ (i === words.length - 1) ? i = 0 : i += 1] ).fadeIn(400);
   });
@@ -24,3 +24,25 @@ var options = {
   "grainHeight": 1
 }
 grained("#container", options);
+
+// behance
+
+require(['be'], function(be) {
+be('RQkGMlVceRKA2Zfg7zkPZMaPKsTlcXNk');
+});
+require(['be'], function(be) {
+ // Using promises
+ be('BehanceApiKey')
+ .project.search('illustration')
+ .then(function success(results) {
+   console.log(results);
+ }, function failure(error) {
+   console.error(error);
+ });
+
+ // Using callbacks
+ be.project.search('branding', function success(results) {
+   console.log(results);
+ });
+});
+
